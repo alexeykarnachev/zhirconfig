cd4b18435bae8b52b118b77ae5c07df3c9b275b4
Alexey Karnachev <alekseykarnachev@gmail.com>
2022-11-22 14:23:15 +0400

    fix vim install

--- vim/install_vim_master.sh
+++ vim/install_vim_master.sh
@@ -1,25 +1,21 @@
 sudo apt update
-sudo apt install git
-sudo apt install make
-sudo apt install clang
-sudo apt install libtool-bin
+sudo apt install -y git make clang libtool-bin
 
 git clone https://github.com/vim/vim.git --single-branch --depth 1
 cd vim/src
 make
 
 make test
-
 sudo make install
 
 # Add X windows clipboard support (also needed for GUI):
-sudo apt install libxt-dev
+sudo apt install -y libxt-dev
 make reconfig
 
 # Add GUI support:
-sudo apt install libgtk-3-dev
+sudo apt install -y libgtk-3-dev
 make reconfig
 
 # Copy vim executable, settings and colorscheme
-sudo cp vim/src/vim /usr/bin/vim
-cp ../home/.vim* $HOME
+sudo cp ./vim/src/vim /usr/bin/vim
+cp -r ../home/.vim* $HOME
